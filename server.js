d491f4ce36542e11b6953be6aae3683f82a8b2f0
Gaurav Umrani <gaurav@okhlee.com>
2018-02-01 19:25:50 +0530

    first bot commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/',(req,res)=>{
+    res.end('Hello')
+})
+app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
